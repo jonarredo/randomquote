c541505da55b5aae42f2074cf122cff2b07cd30f
jonarredo <jonarredo22@gmail.com>
2021-04-20 09:52:30 -0700

    Added more data

--- randomquote.js
+++ randomquote.js
@@ -13,7 +13,20 @@
 // Data structure for project
 
 const data = {
-    quote: ['The supreme art of war is to subdue the enemy without fighting.'],
-    author: ['Billie Eilish'],
-    dates: ['486 BC - 524 BC']
+    quote: ['The supreme art of war is to subdue the enemy without fighting.',
+            'You have power over your mind - not outside events. Realize this, and you will find strength.',
+            'Death smiles at us all, but all a man can do is smile back.',
+            'Never put off till to-morrow what you can do to-day.',
+            'Never spend money before you have it.',
+            'Love your enemies, for they tell you your faults.',
+            'No gains without pains.',
+            'Never interrupt your enemy when he is making a mistake.',
+            'Ability is nothing wihtout opportunity'
+            ],
+    author: ['Billie Eilish', 'Jake Paul',
+             'SpongeBob Squarepants', 'Kim Kardashion',
+             'Bad Bunny', 'T-Pain'],
+    dates: ['486 BC - 524 BC', '121 AD - 180 AD', 
+            '2000 AD - Present', '1915 AD - 2000 AD', 
+            '1200 BC - 1150 BC', '1402 AD - 1464 AD']
 }
