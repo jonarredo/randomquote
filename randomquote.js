8c9eeb35e18ad8454c0d8c2fbd5651915635ef6c
jonarredo <jonarredo22@gmail.com>
2021-04-19 17:37:16 -0700

    Added description and data structure

--- randomquote.js
+++ randomquote.js
@@ -3,6 +3,17 @@
 // Mixed messages project from codecademy
 // Going to keep track of changes using git
 // As well as committing it to my github account
+//
+// The goal of this project is to push out random famous quotes 
+// with a silly author name such as
+//
+// 'The supreme art of war is to subdue the enemy without fighting.'
+// - Billie Eiliesh (486 BC - 524 BC)
+//
+// Data structure for project
 
-console.log('Mixed Messages!');
-
+const data = {
+    quote: ['The supreme art of war is to subdue the enemy without fighting.'],
+    author: ['Billie Eilish'],
+    dates: ['486 BC - 524 BC']
+}
