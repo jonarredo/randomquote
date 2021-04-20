0f2e4bff3177172d7ee374f2105f4b76a386dd6c
jonarredo <jonarredo22@gmail.com>
2021-04-19 17:29:42 -0700

    First commit

---
+++ randomquote.js
@@ -0,0 +1,8 @@
+// randomquote.js
+// 
+// Mixed messages project from codecademy
+// Going to keep track of changes using git
+// As well as committing it to my github account
+
+console.log('Mixed Messages!');
+
